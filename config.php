7ce43655518c50299e86dbbe068ea26c572016ad
opsdevv <opsedev@gmail.com>
2026-05-16 02:24:11 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 // Replace with your Supabase project details
 
-define('SUPABASE_URL', 'https://YOUR_PROJECT.supabase.co');
-define('SUPABASE_API_KEY', 'YOUR_SUPABASE_ANON_KEY');
+define('SUPABASE_URL', 'https://iyruxvbwsvdhvlcczikj.supabase.co');
+define('SUPABASE_API_KEY', 'SUPABASE_SERVICE_ROLE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5cnV4dmJ3c3ZkaHZsY2N6aWtqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODg1NzAxMCwiZXhwIjoyMDk0NDMzMDEwfQ.43Db7OCApy3wYtHWjmXvJxH7THMM7jY9jTm1eFRHlGU');
 
 ?>
